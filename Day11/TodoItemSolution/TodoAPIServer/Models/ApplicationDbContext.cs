3f12d09c64ebdf676790d5dcec25fbb8a3bfa8db
YunBeomOh <dbsqja353@naver.com>
2023-05-31 12:29:11 +0900

    23_05_31 Day84
    RestAPI

---
+++ Day11/TodoItemSolution/TodoAPIServer/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPIServer.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
